54e946657abd8a25102edfc8a1461573b86d3bd5
Daniel Johnston <dan8720@hotmail.com>
2017-12-10 16:42:11 +0000

    complete react project template with asset loading in now aswell

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import '../css/style.css'; // Import CSS -> ADDED IN THIS STEP
+import '../css/style.css';
+
+import keenImage from '../assets/keen.png'; // Importing image -> ADDED IN THIS STEP
 
 export default class Hello extends Component {
   render() {
     return (
       <div>
         Hello from react
+
+        {/* ADDED IN THIS STEP */}
+        <img src={ keenImage } alt='Commander Keen' />
       </div>
     );
   }
